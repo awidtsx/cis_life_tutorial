d3cc90789b45565fd0972b3df5998c4133afdb18
John Eric Diwa <johnericdiwa@Ansells-MacBook-Pro.local>
2026-01-22 09:17:43 +0800

    8th push

--- app/controllers/coop_memberships_controller.rb
+++ app/controllers/coop_memberships_controller.rb
@@ -3,22 +3,36 @@ class CoopMembershipsController < ApplicationController
 
   # GET /coop_memberships or /coop_memberships.json
   def index
-    @coop_memberships = CoopMembership.all
+    @coop_memberships = CoopMembership.includes(
+      cooperative: [:region, :province, :municipal, :barangay],
+      individual: [],
+      insurance_contracts: []
+    ).order(created_at: :desc)
   end
 
   # GET /coop_memberships/1 or /coop_memberships/1.json
   def show
-    @coop_memberships = CoopMembership.all
+    @coop_membership = CoopMembership.includes(
+      cooperative: [:region, :province, :municipal, :barangay, :coop_memberships],
+      individual: [],
+      insurance_contracts: [:agreement]
+    ).find(params[:id])
   end
 
   # GET /coop_memberships/new
   def new
     @coop_membership = CoopMembership.new
+    
+    # Pre-populate from params if provided
+    @coop_membership.cooperative_id = params[:cooperative_id] if params[:cooperative_id]
+    @coop_membership.individual_id = params[:individual_id] if params[:individual_id]
+    
     load_ci
   end
 
   # GET /coop_memberships/1/edit
   def edit
+    load_ci
   end
 
   # POST /coop_memberships or /coop_memberships.json
@@ -27,7 +41,7 @@ class CoopMembershipsController < ApplicationController
 
     respond_to do |format|
       if @coop_membership.save
-        format.html { redirect_to @coop_membership, notice: "Coop membership was successfully created." }
+        format.html { redirect_to @coop_membership, notice: "Cooperative membership was successfully created." }
         format.json { render :show, status: :created, location: @coop_membership }
       else
         load_ci
@@ -41,9 +55,10 @@ class CoopMembershipsController < ApplicationController
   def update
     respond_to do |format|
       if @coop_membership.update(coop_membership_params)
-        format.html { redirect_to @coop_membership, notice: "Coop membership was successfully updated.", status: :see_other }
+        format.html { redirect_to @coop_membership, notice: "Cooperative membership was successfully updated.", status: :see_other }
         format.json { render :show, status: :ok, location: @coop_membership }
       else
+        load_ci
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @coop_membership.errors, status: :unprocessable_entity }
       end
@@ -55,7 +70,7 @@ class CoopMembershipsController < ApplicationController
     @coop_membership.destroy!
 
     respond_to do |format|
-      format.html { redirect_to coop_memberships_path, notice: "Coop membership was successfully destroyed.", status: :see_other }
+      format.html { redirect_to coop_memberships_path, notice: "Cooperative membership was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
@@ -67,25 +82,9 @@ class CoopMembershipsController < ApplicationController
     end
     
     def load_ci
-      @cooperatives = Cooperative.all
-      @individuals  = Individual.all
+      @cooperatives = Cooperative.includes(:region, :province, :municipal, :barangay).order(:name)
+      @individuals  = Individual.order(:last_name, :first_name)
     end
-
-
-    def get_cooperative
-    @cooperatives = Cooperative.where(cooperative_id: params[:cooperative_id])
-    respond_to do |format|
-      format.json { render json: @cooperatives }
-      end
-    end
-
-    def get_individual
-    @individuals = Individual.where(individual_id: params[:individual_id])
-    respond_to do |format|
-      format.json { render json: @individuals }
-      end
-    end
-
 
     # Only allow a list of trusted parameters through.
     def coop_membership_params
--- app/controllers/cooperatives_controller.rb
+++ app/controllers/cooperatives_controller.rb
@@ -3,13 +3,16 @@ class CooperativesController < ApplicationController
 
   # GET /cooperatives or /cooperatives.json
   def index
-    @cooperatives = Cooperative.all
-
+    @cooperatives = Cooperative.includes(:region, :province, :municipal, :barangay, :coop_memberships).all
   end
 
   # GET /cooperatives/1 or /cooperatives/1.json
   def show
-        @cooperatives = Cooperative.all
+    @cooperative = Cooperative.includes(
+      :region, :province, :municipal, :barangay,
+      coop_memberships: :individual,
+      contracts: :insurance_product
+    ).find(params[:id])
   end
 
   # GET /cooperatives/new
@@ -20,6 +23,7 @@ class CooperativesController < ApplicationController
 
   # GET /cooperatives/1/edit
   def edit
+    # Location data is already loaded by existing location relationships
   end
 
   # POST /cooperatives or /cooperatives.json
@@ -38,12 +42,6 @@ class CooperativesController < ApplicationController
     end
   end
 
-  def load_location
-  @regions     = AddRegion.order(:name)
-  @provinces   = AddProvince.none
-  @municipals  = AddMunicipal.none
-  @barangays   = AddBarangay.none
-  end
   # PATCH/PUT /cooperatives/1 or /cooperatives/1.json
   def update
     respond_to do |format|
@@ -67,22 +65,23 @@ class CooperativesController < ApplicationController
     end
   end
 
+  # AJAX endpoints for cascading dropdowns
   def provinces_for_region
-    @provinces = AddProvince.where(add_region_id: params[:add_region_id])
+    @provinces = AddProvince.where(add_region_id: params[:add_region_id]).order(:name)
     respond_to do |format|
       format.json { render json: @provinces }
     end
   end
 
   def municipals_for_province
-    @municipals = AddMunicipal.where(add_province_id: params[:add_province_id])
+    @municipals = AddMunicipal.where(add_province_id: params[:add_province_id]).order(:name)
     respond_to do |format|
       format.json { render json: @municipals }
     end
   end
 
   def barangays_for_municipal
-    @barangays = AddBarangay.where(add_municipal_id: params[:add_municipal_id])
+    @barangays = AddBarangay.where(add_municipal_id: params[:add_municipal_id]).order(:name)
     respond_to do |format|
       format.json { render json: @barangays }
     end
@@ -92,6 +91,13 @@ class CooperativesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_cooperative
       @cooperative = Cooperative.find(params[:id])
+    end
+
+    def load_location
+      @regions     = AddRegion.order(:name)
+      @provinces   = AddProvince.none
+      @municipals  = AddMunicipal.none
+      @barangays   = AddBarangay.none
     end
 
     # Only allow a list of trusted parameters through.
